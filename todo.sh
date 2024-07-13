88a17b74c4d5d65cbe41426229cd6b412ccafdf6
TypicalAM <tpam_piaseczny@protonmail.com>
2024-07-13 03:48:06 +0200

    feat: more sensible todo launching

--- todo.sh
+++ todo.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Opens the nvim todo list in the most recent kitty window
+# Opens the nvim todo list
 
 ensure_available() {
 	command -v "${1}" >/dev/null 2>&1 || {
@@ -10,25 +10,12 @@ ensure_available() {
 }
 
 main() {
-	local highest_number=0
-	local highest_filename=""
-
-	for file in /tmp/kitty_dev*; do
-		number="${file##*_dev}"
-		if ((number > highest_number)); then
-			highest_number="$number"
-			highest_filename="$file"
-		fi
-	done
-
-	if [ -n "$highest_filename" ]; then
-		base_command="kitty @ --to unix:$highest_filename"
-		$base_command launch --type tab
-		$base_command send-text nvim \\x0d
-		$base_command send-text t\\x0d
-	fi
+	i3-msg exec 'kitty -- nvim /home/adam/notes/luzne\ notatki/todo.md'
+	sleep 0.3
+	i3-msg resize shrink width 30 px or 30 ppt
 }
 
 ensure_available "kitty"
+ensure_available "i3-msg"
 
 main
